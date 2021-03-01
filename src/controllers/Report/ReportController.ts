099bf77896a9494f972133052188c133cb35fc73
Artiom Oganesyan <artiomoganesyan@gmail.com>
2021-03-01 14:56:44 +0300

    all viewIDs

--- src/controllers/Report/ReportController.ts
+++ src/controllers/Report/ReportController.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from "express";
-import { signIntoGoogle } from "../../api/google";
+import { signIntoGoogle, getAllSites } from "../../api/google";
 import { writeFileCSV } from "../../tools/fs/fs";
 import { CrudController } from "../CrudController";
 import { documentCreation } from "../../tools/documentCreation";
 import { dateValidation } from "../../tools/validation/dateValidation";
 import { ICombined } from "../../config/types";
-import { GoogleApis } from "googleapis";
 import { errorCreation } from "../../tools/errorCreation";
 
 export class ReportController extends CrudController {
@@ -28,13 +27,13 @@ export class ReportController extends CrudController {
   ) {
     try {
       const analyticsreporting = signIntoGoogle();
-      if (!req.body.date_ranges || !req.body.view_ids) {
+      const sitesInfo: { id: string; URL: string }[] = [];
+      if (!req.body.date_ranges) {
         throw "request body is not valid";
       }
       const { startDate, endDate } = req.body.date_ranges[0];
-      const viewIDs = req.body.view_ids;
 
-      if (!startDate || !endDate || !viewIDs.length) {
+      if (!startDate || !endDate) {
         throw "request body is not valid";
       }
 
@@ -50,90 +49,100 @@ export class ReportController extends CrudController {
       let combined: ICombined = {};
       let CSVString = "";
 
-      const promiseArr = viewIDs.map(async (viewID: string, index: number) => {
-        try {
-          const data = await analyticsreporting.reports.batchGet({
-            requestBody: {
-              reportRequests: [
-                {
-                  viewId: viewID,
-                  dateRanges: [
-                    {
-                      endDate: endDate,
-                      startDate: startDate,
-                    },
-                  ],
-                  metrics: [
-                    {
-                      expression: "ga:users",
-                    },
-                  ],
-                  dimensions: [
-                    {
-                      name: "ga:userGender",
-                    },
-                  ],
-                },
-                {
-                  viewId: viewID,
-                  dateRanges: [
-                    {
-                      endDate: endDate,
-                      startDate: startDate,
-                    },
-                  ],
-                  metrics: [
-                    {
-                      expression: "ga:users",
-                    },
-                  ],
-                  dimensions: [
-                    {
-                      name: "ga:userAgeBracket",
-                    },
-                  ],
-                },
-              ],
-            },
-          });
-          return data;
-        } catch (error) {
-          throw { msg: "google requestBody error", error: { ...error } };
-        }
-      });
-
-      Promise.all(promiseArr)
-        .then((wholeResponse) => {
-          wholeResponse.forEach((response: any, index) => {
-            if (response && response.data && response.data.reports) {
-              combined = {
-                site: "needs implementation",
-                viewID: viewIDs[index],
-                startDate: startDate,
-                endDate: endDate,
-              };
-            }
-
-            const toCombine = documentCreation.responseCombining(
-              response.data.reports
-            );
-
-            combined = {
-              ...combined,
-              ...toCombine,
-            };
-
-            CSVString = documentCreation.CSVCreation(combined, CSVString);
-          });
-        })
-        .then(() => {
-          writeFileCSV(__dirname + "/doc.csv", CSVString);
+      getAllSites()
+        .then((sites: any) => {
+          if (sites.data.items) {
+            sites.data.items.forEach((site: any) => {
+              sitesInfo.push({ id: site.id, URL: site.websiteUrl });
+            });
+          }
         })
         .finally(() => {
-          res.sendFile(__dirname + "/doc.csv");
-        })
-        .catch((err) => {
-          res.status(err.error.code).json({ error: err.error.errors });
+          const promiseArr = sitesInfo.map(async (site: any, index: number) => {
+            try {
+              const data = await analyticsreporting.reports.batchGet({
+                requestBody: {
+                  reportRequests: [
+                    {
+                      viewId: site.id,
+                      dateRanges: [
+                        {
+                          endDate: endDate,
+                          startDate: startDate,
+                        },
+                      ],
+                      metrics: [
+                        {
+                          expression: "ga:users",
+                        },
+                      ],
+                      dimensions: [
+                        {
+                          name: "ga:userGender",
+                        },
+                      ],
+                    },
+                    {
+                      viewId: site.id,
+                      dateRanges: [
+                        {
+                          endDate: endDate,
+                          startDate: startDate,
+                        },
+                      ],
+                      metrics: [
+                        {
+                          expression: "ga:users",
+                        },
+                      ],
+                      dimensions: [
+                        {
+                          name: "ga:userAgeBracket",
+                        },
+                      ],
+                    },
+                  ],
+                },
+              });
+              return data;
+            } catch (error) {
+              throw { msg: "google requestBody error", error: { ...error } };
+            }
+          });
+
+          Promise.all(promiseArr)
+            .then((wholeResponse) => {
+              wholeResponse.forEach((response: any, index) => {
+                if (response && response.data && response.data.reports) {
+                  combined = {
+                    site: sitesInfo[index].URL,
+                    viewID: sitesInfo[index].id,
+                    startDate: startDate,
+                    endDate: endDate,
+                  };
+                }
+
+                const toCombine = documentCreation.responseCombining(
+                  response.data.reports
+                );
+
+                combined = {
+                  ...combined,
+                  ...toCombine,
+                };
+
+                CSVString = documentCreation.CSVCreation(combined, CSVString);
+              });
+            })
+            .then(() => {
+              writeFileCSV(__dirname + "/doc.csv", CSVString);
+            })
+            .finally(() => {
+              res.sendFile(__dirname + "/doc.csv");
+            })
+            .catch((err) => {
+              res.status(err.error.code).json({ error: err.error.errors });
+            });
         });
     } catch (error) {
       res.json(errorCreation([error]));
